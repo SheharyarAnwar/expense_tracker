5cd50332fd37bfaaa06772410974c194345dbf0b
Sherry_XYZ <sheharyar28@hotmail.com>
2020-11-24 23:26:11 +0500

    Fixed the react warning causing CI error

--- src/Components/Balance/index.js
+++ src/Components/Balance/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 export default function Index() {
-  const [balance, setbalance] = useState(0);
+  //const [balance, setbalance] = useState(0);
   return (
     <div>
       <p>Your Balance</p>
-      <p style={{ marginTop: "10px" }}>{balance}</p>
+      <p style={{ marginTop: "10px" }}>{350}</p>
     </div>
   );
 }
